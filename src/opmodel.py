ee46b34245af8c0407b3ee9802d32c2323cf56cc
Eirol <74545840+eirol@users.noreply.github.com>
2022-06-13 22:28:05 +0200

    Update test

--- src/opmodel.py
+++ src/opmodel.py
@@ -5,6 +5,8 @@ import pandas as pd
 import math
 import traceback
 
+print("Model")
+
 class SimulateTakeOff():
   """ Class to run a simulation of how automation and the economy 
       will feed into each other.
