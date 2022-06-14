525c8adf9165ab6b05deda2fbed41b8e553e9986
Eirol <74545840+eirol@users.noreply.github.com>
2022-06-14 16:24:37 +0200

    import plt

--- opmodel/analysis/__init__.py
+++ opmodel/analysis/__init__.py
@@ -1,4 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+
 from ..core.opmodel import SimulateTakeOff
 from ..core.utils import display
--- opmodel/core/utils.py
+++ opmodel/core/utils.py
@@ -4,8 +4,8 @@ Utilities.
 
 # Import display in non IPython environments
 try:
-    from IPython.display import display
+  from IPython.display import display
 except ModuleNotFoundError: 
-    # As a fallback, just print
-    def display(x):
-        print(x)
+  # As a fallback, just print
+  def display(x):
+    print(x)
