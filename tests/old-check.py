fbe065382b8717a29ca656a5a466fb1bb9a4b57b
Eirol <74545840+eirol@users.noreply.github.com>
2022-09-04 23:39:31 +0200

    add plain text olde sheet comparison

---
+++ tests/old-check.py
@@ -0,0 +1,164 @@
+import re
+import os
+import inspect
+import argparse
+import pandas as pd
+from openpyxl import load_workbook
+from opmodel.core.utils import *
+
+parser = argparse.ArgumentParser()
+parser.add_argument('local_olde_sheet_path')
+args = parser.parse_args()
+
+PARAMETER_TABLE_URL = 'https://docs.google.com/spreadsheets/d/1r-WxW4JeNoi_gCMc5y2iTlJQnan_LLCF5s_V4ZDDMkI/edit#gid=777689843'
+OLDE_SHEET_PATH = args.local_olde_sheet_path
+
+# -----------------------------------------------------------------------------
+# Modifify SimulateTakeOff
+# -----------------------------------------------------------------------------
+
+from opmodel.core.opmodel import *
+
+# Get source code
+code = inspect.getsource(SimulateTakeOff)
+
+# Modify the code (hackily)
+modified_code = []
+lines = iter(code.splitlines())
+for line in lines:
+  # Get rid of asserts
+  if re.match(r'^\s*assert', line):
+    line = re.sub(r'(^\s*)', r'\1if False: ', line)
+
+  # Allow infinite ceilings
+  if re.match(r'^\s*def _update_rnd\(', line):
+    # Find our target line
+    while not re.match('^\s*ceiling_penalty =', line):
+      modified_code.append(line)
+      line = next(lines)
+    line = re.sub(r'(\s*ceiling_penalty =)', r'\1 1 if performance_ceiling == np.inf else ', line)
+
+  modified_code.append(line)
+
+modified_code = '\n'.join(modified_code)
+
+# Optionally save the modified code
+if True:
+  with open(os.path.join(get_option('report_dir'), 'SimulateTakeOff_modified.py'), 'w') as f:
+    f.write(modified_code)
+
+# Load the modified code
+exec(modified_code)
+
+# -----------------------------------------------------------------------------
+# Load Ye Olde Sheet
+# -----------------------------------------------------------------------------
+olde_workbook = load_workbook(OLDE_SHEET_PATH, data_only = True)
+olde_sheet = olde_workbook['CES production function']
+
+for c in reversed(olde_sheet['A']):
+  if c.value is not None:
+    last_year = c.value
+    last_row = c.row
+    break
+
+# -----------------------------------------------------------------------------
+# Simulation
+# -----------------------------------------------------------------------------
+
+# Retrieve parameter estimates from spreadsheet
+set_parameter_table_url(PARAMETER_TABLE_URL)
+parameter_table = get_parameter_table()
+parameters = {parameter : row['Best guess'] for parameter, row in parameter_table.iterrows()}
+
+# Convert strings to floats
+for p, v in parameters.items():
+  if isinstance(v, str):
+    if v.endswith('%'):
+      parameters[p] = float(v[:-1])/100
+    else:
+      parameters[p] = float(v)
+
+parameters['n_labour_tasks'] = 40
+parameters['t_end'] = last_year + 1
+
+model = SimulateTakeOff(**parameters, t_step=1)
+
+# Override the automation training FLOP for R&D
+model.automation_training_flops_rnd = 10**np.array([float(c.value) for row in olde_sheet['DI7:EV7'] for c in row])
+model.automation_training_flops_rnd = np.insert(model.automation_training_flops_rnd, 0, 1.0)
+
+model.run_simulation()
+
+# -----------------------------------------------------------------------------
+# Compare the results
+# -----------------------------------------------------------------------------
+def load_range(range):
+  to_float = lambda x: np.nan if (x is None or x == '#REF!') else float(x)
+
+  if re.match(r'^[A-Z]+$', range):
+    col = range
+    range = f'{col}54:{col}{last_row}'
+
+  if ':' in range:
+    # This is a real range
+    array = []
+    for row in olde_sheet[range]:
+      for cell in row:
+        array.append(to_float(cell.value))
+    return np.array(array)
+  else:
+    # This is just a single cell
+    return to_float(olde_sheet[range].value)
+
+olde = {
+  'agi_year'                   : load_range('BA54'),
+  'full_rnd_automation_year'   : load_range('BB54'),
+  'fast_growth_year'           : load_range('BC54'),
+  'gwp'                        : load_range('P'),
+  'compute'                    : 10**load_range('G'),
+  'software'                   : load_range('AM'),
+  'biggest_training_run'       : 10**load_range('BT'),
+  'hardware_performance'       : 10**load_range(f'AD53:AD{last_row-1}'),
+}
+
+modern = {
+  'agi_year'                  : float(model.index_to_time(np.argmax(model.frac_tasks_automated_goods))),
+  'full_rnd_automation_year'  : float(model.index_to_time(np.argmax(model.frac_tasks_automated_rnd))),
+  'fast_growth_year'          : float(model.index_to_time((np.argmax(np.log(model.gwp[1:]/model.gwp[:-1]) > 0.20)))),
+  'gwp'                       : model.gwp,
+  'compute'                   : model.compute,
+  'software'                  : model.software,
+  'biggest_training_run'      : model.biggest_training_run,
+  'hardware_performance'      : model.hardware_performance,
+}
+
+year_variables = {
+  'AGI': 'agi_year',
+  'Full R&D automation': 'full_rnd_automation_year',
+  'First 20% growth': 'fast_growth_year',
+}
+
+state_variables = {
+  'GWP': 'gwp',
+  'Compute': 'compute',
+  'Biggest training run': 'biggest_training_run',
+  'Hardware performance': 'hardware_performance',
+}
+
+for label, var in year_variables.items():
+  #print(f'{label}: {olde[var]} - {modern[var]}')
+
+  print(f'{label}')
+  print(f'  {olde[var]}')
+  print(f'  {modern[var]}')
+  print()
+
+for label, var in state_variables.items():
+  print(label)
+  for i, year in enumerate(model.timesteps):
+    #print(f'  {year}: {olde[var][i]} - {modern[var][i]}')
+    print(f'  {year}: {olde[var][i]:.6e}')
+    print(f'        {modern[var][i]:.6e}')
+  print()
+
